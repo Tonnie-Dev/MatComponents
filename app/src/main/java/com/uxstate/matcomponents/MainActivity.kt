8757e4c5ec631533796963daa907162949c4e8ee
Antony <vontonnie@gmail.com>
2021-08-08 19:49:45 +0300

    Add PhotographerCard Composable

--- app/src/main/java/com/uxstate/matcomponents/MainActivity.kt
+++ app/src/main/java/com/uxstate/matcomponents/MainActivity.kt
@@ -3,10 +3,11 @@ package com.uxstate.matcomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.matcomponents.ui.theme.MatComponentsTheme
 
@@ -17,7 +18,7 @@ class MainActivity : ComponentActivity() {
             MatComponentsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                   
+                    PhotographerCard()
                 }
             }
         }
@@ -25,11 +26,28 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Composable
+fun PhotographerCard() {
+
+    Column {
+
+
+        Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+
+        //LocalContentAlpha is defining opacity level of its children
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+
+
+            Text(text = "3 Minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MatComponentsTheme {
-
+        PhotographerCard()
     }
 }
