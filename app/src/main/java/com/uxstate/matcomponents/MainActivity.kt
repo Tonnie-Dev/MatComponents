ef2f3ee8873d86256164f9ca4e5f4d265c0809cf
Antony <vontonnie@gmail.com>
2021-08-08 19:26:37 +0300

    Update Libs

--- app/src/main/java/com/uxstate/matcomponents/MainActivity.kt
+++ app/src/main/java/com/uxstate/matcomponents/MainActivity.kt
@@ -3,10 +3,11 @@ package com.uxstate.matcomponents
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.matcomponents.ui.theme.MatComponentsTheme
 
@@ -17,22 +18,42 @@ class MainActivity : ComponentActivity() {
             MatComponentsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                   PhotographerCard()
                 }
             }
         }
     }
+
+   
 }
 
-@Composable
+/*@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}*/
+
+@Composable
+fun PhotographerCard() {
+    
+    Column {
+        
+        Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+
+        //LocalContentApha is defining the opacity level of Children 
+        
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+           
+            Text(text = "3 minutes ago", style = MaterialTheme.typography.body1)
+        }
+    }
+    
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MatComponentsTheme {
-        Greeting("Android")
+        PhotographerCard()
     }
 }
