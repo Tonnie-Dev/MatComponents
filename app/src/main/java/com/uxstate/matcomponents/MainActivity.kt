2939c5faf99fdea0a1c78faffc562e2c335ef9eb
Antony <vontonnie@gmail.com>
2021-08-11 08:17:49 +0300

    Add PlaceHolder Surface

--- app/src/main/java/com/uxstate/matcomponents/MainActivity.kt
+++ app/src/main/java/com/uxstate/matcomponents/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.uxstate.matcomponents.ui.theme.MatComponentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,16 +34,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PhotographerCard() {
 
-    Column {
+    Row {
+
+        Surface(modifier = Modifier.size(50.dp), shape = CircleShape, color = MaterialTheme
+            .colors.onSecondary.copy(alpha = 0.2F)){}
 
 
-        Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
-
-        //LocalContentAlpha is defining opacity level of its children
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        Column {
 
 
-            Text(text = "3 Minutes ago", style = MaterialTheme.typography.body2)
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+
+            //LocalContentAlpha is defining opacity level of its children
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+
+
+                Text(text = "3 Minutes ago", style = MaterialTheme.typography.body2)
+            }
         }
     }
 }
