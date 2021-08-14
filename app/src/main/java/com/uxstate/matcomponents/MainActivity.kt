ac18dde444201336b82ad7b1bd47a19de1236ec1
Antony <vontonnie@gmail.com>
2021-08-14 19:11:37 +0300

    Remove UI Tooling Preview Lib

--- app/src/main/java/com/uxstate/matcomponents/MainActivity.kt
+++ app/src/main/java/com/uxstate/matcomponents/MainActivity.kt
@@ -5,30 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.sharp.Favorite
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import com.uxstate.matcomponents.ui.theme.MatComponentsTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +24,17 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+
+    @Composable
+    fun ImageList() {
+Row(verticalAlignment = Alignment.CenterVertically){
+
+   // val painter = rememberImagePainter()
+   // Image(painter = , contentDescription = )
+}
+
     }
 
 }
